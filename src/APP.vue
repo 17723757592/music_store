f1566ee6beea2d3b0918c4e71c1df8e115b239e0
liangbaikai_w <2407120494@qq.com>
2023-09-01 14:01:12 +0000

    add src/APP.vue.
    Signed-off-by: liangbaikai_w <2407120494@qq.com>

---
+++ src/APP.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">  
+    <BannerName></BannerName>
+    <router-view :key="$route.fullPath"><HomePage></HomePage></router-view>
+    <BottomName></BottomName>
+  </div>
+</template>
+
+<script>
+import BannerName from './components/Banner.vue'
+import HomePage from './components/home/HomePage.vue'
+import BottomName from './components/Bottom'
+
+import { mapActions,mapState } from 'vuex'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+    }
+  },
+  methods: {
+    ...mapActions('home', {addMessage:'addMessage'})
+  },
+  computed:{
+    ...mapState('cart', ['shoppingCart'])
+  },
+  created() {
+    this.addMessage()
+  },
+  components: {
+    BannerName,
+    HomePage,
+    BottomName
+  },
+  watch:{
+    shoppingCart:{
+      immediate:true,
+      deep:true,
+      handler(val){
+        sessionStorage.setItem('shoppingCart', JSON.stringify(val))
+      }
+    }
+  },
+  mounted(){
+
+  }
+}
+</script>
+
+<style>
+li{
+    list-style: none;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+body, html, h1, h2, h3, h4, h5, h6, ul, ol, li, dl, dt, dd, header, menu, section, p, input, td, th, section{
+  padding: 0;
+  margin: 0;
+}
+a{
+  cursor: pointer;
+  text-decoration: none;
+  color: #333;
+  padding: 0;
+  margin: 0;
+}
+</style>
